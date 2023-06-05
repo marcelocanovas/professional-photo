428402f5c914fa36636637875e5624b1521b71e9
marcelocanovas <2595289+marcelocanovas@users.noreply.github.com>
2023-06-05 03:42:08 +0000

    changed model to Photo Realistic

--- app/api/submit-remix/route.ts
+++ app/api/submit-remix/route.ts
@@ -16,8 +16,18 @@ async function createFormData(image: File, prompt: string) {
   return formData;
 }
 
+// Model ID's
+// Stable Diffusion 1.5	8b1b897c-d66d-45a6-b8d7-8e32421d02cf	A red bird
+// Stable Diffusion 2.1	ee88d150-4259-4b77-9d0f-090abe29f650	A red bird
+// OpenJourney v4	1e7737d7-545e-469f-857f-e4b46eaa151d	A red bird
+// OpenJourney v2	d66b1686-5e5d-43b2-a2e7-d295d679917c	A red bird
+// OpenJourney v1	7575ea52-3d4f-400f-9ded-09f7b1b1a5b8	A red bird mdjrny-v4 style
+// Modern Disney	8ead1e66-5722-4ff6-a13f-b5212f575321	A red bird modern disney style
+// Future Diffusion	1285ded4-b11b-4993-a491-d87cdfe6310c	A red bird future style
+// Realistic Vision v2.0	eab32df0-de26-4b83-a908-a83f3015e971	A red bird
+
 async function postImageToApi(formData: FormData) {
-  const modelId = "1e7737d7-545e-469f-857f-e4b46eaa151d";
+  const modelId = "eab32df0-de26-4b83-a908-a83f3015e971	";
   const apiUrl = `https://api.tryleap.ai/api/v1/images/models/${modelId}/remix`;
 
   const response = await fetch(apiUrl, {
